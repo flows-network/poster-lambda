f157562e63f726a0c5881bc8207e1203459f4420
DarumaDocker <DarumaDocker@gmail.com>
2023-09-15 13:04:01 +0800

    test round corner

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -80,14 +80,59 @@ fn draw_text(mut img: image::DynamicImage, text: &str, far_left: u32, far_top: u
     return buf;
 }
 
+fn draw_filled_circle_mut<C>(canvas: &mut C, center: (i32, i32), radius: i32, color: C::Pixel)
+where
+    C: drawing::Canvas,
+    C::Pixel: 'static,
+{
+    let mut x = 0i32;
+    let mut y = radius;
+    let mut p = 1 - radius;
+    let x0 = center.0;
+    let y0 = center.1;
+
+    while x <= y {
+        drawing::draw_line_segment_mut(
+            canvas,
+            ((x0 - x) as f32, (y0 + y) as f32),
+            ((x0 + x) as f32, (y0 + y) as f32),
+            color,
+        );
+        // drawing::draw_line_segment_mut(
+        //     canvas,
+        //     ((x0 - y) as f32, (y0 + x) as f32),
+        //     ((x0 + y) as f32, (y0 + x) as f32),
+        //     color,
+        // );
+        // drawing::draw_line_segment_mut(
+        //     canvas,
+        //     ((x0 - x) as f32, (y0 - y) as f32),
+        //     ((x0 + x) as f32, (y0 - y) as f32),
+        //     color,
+        // );
+        // drawing::draw_line_segment_mut(
+        //     canvas,
+        //     ((x0 - y) as f32, (y0 - x) as f32),
+        //     ((x0 + y) as f32, (y0 - x) as f32),
+        //     color,
+        // );
+
+        x += 1;
+        if p < 0 {
+            p += 2 * x + 1;
+        } else {
+            y -= 1;
+            p += 2 * (x - y) + 1;
+        }
+    }
+}
+
 fn draw_avatar(body: Vec<u8>, width: u32, height: u32, left: u32, top: u32) -> image::DynamicImage {
     let mut avatar = image::load_from_memory(&body).unwrap();
     avatar = avatar.resize(width, height, image::imageops::Lanczos3);
 
     let x = width as i32 / 2;
-    for c in 0..20 {
-        drawing::draw_hollow_circle_mut(&mut avatar, (x, x), x + c, image::Rgba([123 as u8; 4]));
-    }
+    draw_filled_circle_mut(&mut avatar, (x, x), x, image::Rgba([123 as u8; 4]));
 
     let mut img = image::load_from_memory(TEMPLATE_BUF).unwrap();
     image::imageops::overlay(&mut img, &avatar, left, top);
